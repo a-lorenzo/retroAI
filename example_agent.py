901ff61b363bbdc8e160c6122f22f6fc95fbb889
Agustin Lorenzo <agustinlorenzo@Agustins-Air.lan>
2023-10-07 00:35:55 -0400

    Added visualization for final network

--- example_agent.py
+++ example_agent.py
@@ -3,30 +3,32 @@ import numpy as np
 import cv2
 import neat
 import pickle
-
+import visualize
 
 env = gym.make("ALE/MsPacman-v5", render_mode="human")
+env.metadata['render_fps'] = 120
 imgarray = []
 
 def eval_genomes(genomes, config):
 
     for genome_id, genome in genomes:
-        ob = np.array(env.reset())
+        ob = np.array(env.reset(), dtype = object)
         ac = env.action_space.sample()
         
         inx, iny, inc = env.observation_space.shape
 
         inx = int(inx/8)
         iny = int(iny/8)
-        print(inx, iny, inc)
         net = neat.nn.recurrent.RecurrentNetwork.create(genome, config)
+        #net.visualize()
+        #visualize.draw_net(config, net, True)
 
         current_max_fitness = 0
         fitness_current = 0
         frame = 0
         counter = 0
-        xpos = 0
-        xpos_max = 0
+        score = 0
+        score_max = 0
 
         done = False
 
@@ -36,14 +38,10 @@ def eval_genomes(genomes, config):
             frame += 1
 
             # Reducing screenshot of emulator
-            #print("Shape of ob:", ob.shape if ob is not None else None)
-            #print("Data type of ob:", ob.dtype if ob is not None else None)
-
+            #scaledimg = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
             #ob = cv2.resize(ob, (inx, iny))
             #ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
             #ob = np.reshape(ob, (inx, iny))
-
-            imgarray.clear()
 
             # Flattening image into 1D array
             for x in ob:
@@ -51,17 +49,28 @@ def eval_genomes(genomes, config):
                     if not isinstance(y[0][0], str):
                         imgarray.append(float(y[0][0]) / 255.0)
 
-
+            
             nnOutput = net.activate(imgarray)
-            print("\n\n", "NEURALNET OUTPUT:\n=============")
-            print(nnOutput)
             
             action = np.argmax(nnOutput)
             result  = env.step(action)
             observation, reward, terminated, truncated, info = env.step(action)
+            imgarray.clear()
+            fitness_current += reward
 
+            # Evaluating fitness with in-game score
+            if fitness_current > current_max_fitness:
+                current_max_fitness = fitness_current
+                counter = 0
+            else:
+                counter += 1
+
+            if done or counter == 250 or terminated or truncated:
+                done = True
+                print("Genome: ", genome_id, "Fitness: ", fitness_current)
+
+            genome.fitness = fitness_current
             
-
             
 
             
@@ -71,5 +80,21 @@ config = neat.Config(neat.DefaultGenome,
                      neat.DefaultStagnation,
                      'config-feedforward')
 
+
 p = neat.Population(config)
-winner = p.run(eval_genomes)
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+p.add_reporter(neat.Checkpointer(10))
+
+# Run the neat algorithm for 10 generations and write the winner
+winner = p.run(eval_genomes, 10)
+print('\nBest genome:\n{!s}'.format(winner))
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner, output, 1)
+
+visualize.draw_net(config, winner, True)
+visualize.plot_stats(stats, ylog=False, view=True)
+visualize.plot_species(stats, view=True)
+
+
